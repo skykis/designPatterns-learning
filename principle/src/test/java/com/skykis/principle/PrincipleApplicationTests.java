1a85422290fa4608c2190be66285b37467303f77
WBX <290548180@qq.com>
2020-07-13 17:04:34 +0800

    初始化子项目-7大设计原则

--- principle/src/test/java/com/skykis/principle/PrincipleApplicationTests.java
+++ principle/src/test/java/com/skykis/principle/PrincipleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.skykis.designPatterns;
+package com.skykis.principle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DesignPatternsApplicationTests {
+class PrincipleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
