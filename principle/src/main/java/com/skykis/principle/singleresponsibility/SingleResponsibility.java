3d6afdad4d741c6abe123318285674944d1df92a
sk <290548180@qq.com>
2020-07-13 20:39:40 +0800

    演示违反单一职责原则

--- principle/src/main/java/com/skykis/principle/singleresponsibility/SingleResponsibility.java
+++
@@ -1,7 +0,0 @@
-package com.skykis.principle.singleresponsibility;
-
-public class SingleResponsibility {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ principle/src/main/java/com/skykis/principle/singleresponsibility/SingleResponsibility1.java
@@ -0,0 +1,25 @@
+package com.skykis.principle.singleresponsibility;
+
+/**
+ * 单一职责原则
+ */
+public class SingleResponsibility1 {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.run("摩托车");
+        vehicle.run("汽车");
+        vehicle.run("飞机");
+    }
+}
+
+/**
+ * 交通工具
+ * 在run方法中，违反了单一职责原则
+ * 解决方案：根据入参vehicle职责不同分解成不同的类 => SingleResponsibility2
+ */
+class Vehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "在公路上运行。");
+    }
+}
