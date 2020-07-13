f4ceb89b49f1a6353328059b561c0ae15b0acb13
WBX <290548180@qq.com>
2020-07-13 16:52:55 +0800

    初始化项目

---
+++ src/test/java/com/skykis/designPatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skykis.designPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
