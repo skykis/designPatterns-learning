7879d6fa49f447c4c0c770780a21e477bba03309
sk <290548180@qq.com>
2020-07-15 00:35:14 +0800

    演示接口隔离原则含义

---
+++ principle/src/main/java/com/skykis/principle/segregation/Segregation1.java
@@ -0,0 +1,112 @@
+package com.skykis.principle.segregation;
+
+interface Interface1 {
+    void operation1();
+
+    void operation2();
+
+    void operation3();
+
+    void operation4();
+
+    void operation5();
+}
+
+/**
+ * 接口隔离原则
+ * Interface1分别相对A和C不是最小接口
+ * 解决方案：拆分
+ */
+public class Segregation1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class B implements Interface1 {
+    @Override
+    public void operation1() {
+        System.out.println("B1");
+    }
+
+    @Override
+    public void operation2() {
+        System.out.println("B2");
+    }
+
+    @Override
+    public void operation3() {
+        System.out.println("B3");
+    }
+
+    @Override
+    public void operation4() {
+        System.out.println("B4");
+    }
+
+    @Override
+    public void operation5() {
+        System.out.println("B5");
+    }
+}
+
+class D implements Interface1 {
+    @Override
+    public void operation1() {
+        System.out.println("D1");
+    }
+
+    @Override
+    public void operation2() {
+        System.out.println("D2");
+    }
+
+    @Override
+    public void operation3() {
+        System.out.println("D3");
+    }
+
+    @Override
+    public void operation4() {
+        System.out.println("D4");
+    }
+
+    @Override
+    public void operation5() {
+        System.out.println("D5");
+    }
+}
+
+/**
+ * A通过Interface1调用B类的1 2 3方法
+ */
+class A {
+    public void depand1(Interface1 i) {
+        i.operation1();
+    }
+
+    public void depand2(Interface1 i) {
+        i.operation2();
+    }
+
+    public void depand3(Interface1 i) {
+        i.operation3();
+    }
+}
+
+/**
+ * C通过Interface1调用D的1 4 5方法
+ */
+class C {
+    public void depand1(Interface1 i) {
+        i.operation1();
+    }
+
+    public void depand4(Interface1 i) {
+        i.operation2();
+    }
+
+    public void depand5(Interface1 i) {
+        i.operation3();
+    }
+}
