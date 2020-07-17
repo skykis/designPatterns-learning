2138f73a31f4ff71dc9d3de1065a53b295abcfb4
sk <290548180@qq.com>
2020-07-18 00:06:14 +0800

    接口隔离原则uml

--- principle/src/main/java/com/skykis/principle/segregation/Segregation.java
+++ principle/src/main/java/com/skykis/principle/segregation/Segregation.java
@@ -1,6 +1,6 @@
 package com.skykis.principle.segregation;
 
-interface Interface1 {
+interface Interface {
     void operation1();
 
     void operation2();
@@ -17,13 +17,13 @@ interface Interface1 {
  * Interface1分别相对A和C不是最小接口
  * 解决方案：拆分
  */
-public class Segregation1 {
+public class Segregation {
     public static void main(String[] args) {
 
     }
 }
 
-class B implements Interface1 {
+class B implements Interface {
     @Override
     public void operation1() {
         System.out.println("B1");
@@ -50,7 +50,7 @@ class B implements Interface1 {
     }
 }
 
-class D implements Interface1 {
+class D implements Interface {
     @Override
     public void operation1() {
         System.out.println("D1");
@@ -81,15 +81,15 @@ class D implements Interface1 {
  * A通过Interface1调用B类的1 2 3方法
  */
 class A {
-    public void depand1(Interface1 i) {
+    public void depand1(Interface i) {
         i.operation1();
     }
 
-    public void depand2(Interface1 i) {
+    public void depand2(Interface i) {
         i.operation2();
     }
 
-    public void depand3(Interface1 i) {
+    public void depand3(Interface i) {
         i.operation3();
     }
 }
@@ -98,15 +98,15 @@ class A {
  * C通过Interface1调用D的1 4 5方法
  */
 class C {
-    public void depand1(Interface1 i) {
+    public void depand1(Interface i) {
         i.operation1();
     }
 
-    public void depand4(Interface1 i) {
+    public void depand4(Interface i) {
         i.operation2();
     }
 
-    public void depand5(Interface1 i) {
+    public void depand5(Interface i) {
         i.operation3();
     }
 }
